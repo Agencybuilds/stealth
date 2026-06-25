3b12c73ca9072557adfd13cd370077a44b4f368c
mxrtins04 <olatunbosunmartins14@gmail.com>
2026-06-25 10:29:53 +0100

    feat(v2): implement core logic for team task board from emails tool

---
+++ tools/v2/team/team-task-board-from-emails/index.ts
@@ -0,0 +1,17 @@
+export {
+  extractTaskFromEmail,
+  groupTasksByStatus,
+  createTaskBoardService,
+} from "./services/taskBoardService";
+
+export type {
+  TaskBoardService,
+} from "./services/taskBoardService";
+
+export type {
+  Email,
+  TaskCard,
+  TaskBoard,
+  LoadState,
+  TaskBoardServiceConfig,
+} from "./types";
